85f1976a4128e1d3feb53500e9dbb338669275b9
Jared Hanson <jaredhanson@gmail.com>
2013-06-03 17:35:07 -0700

    Test case for parsing Lua executable for 32-bit Linux.

--- test/index.test.js
+++ test/index.test.js
@@ -7,8 +7,47 @@ describe('elf', function() {
     expect(elf.load).to.be.a('function');
   });
   
+  describe('loading lua executable on 32-bit Linux', function() {
+    it('should parse correctly', function(done) {
+      elf.load('test/data/lua-5.2.1_Linux26_bin/lua52', function(err, file) {
+        expect(err).to.be.null;
+        
+        /*
+        console.log('type: ' + file.type);
+        console.log('machine: ' + file.machine);
+        console.log('version: ' + file.version);
+        console.log('entry: ' + file.entry);
+        console.log('phoff: ' + file.phoff);
+        console.log('shoff: ' + file.shoff);
+        console.log('flags: ' + file.flags);
+        console.log('ehsize: ' + file.ehsize);
+        console.log('phentsize: ' + file.phentsize);
+        console.log('phnum: ' + file.phnum);
+        console.log('shentsize: ' + file.shentsize);
+        console.log('shnum: ' + file.shnum);
+        console.log('shstrndx: ' + file.shstrndx);
+        */
+        
+        expect(file.enc).to.equal(elf.Encoding.LSB);
+        expect(file.type).to.equal(elf.Type.EXECUTABLE);
+        expect(file.machine).to.equal(elf.Machine.I386);
+        expect(file.version).to.equal(elf.Version.CURRENT);
+        expect(file.entry).to.equal(134524860); // ???
+        expect(file.phoff).to.equal(52);
+        expect(file.shoff).to.equal(134068);
+        expect(file.flags).to.equal(0);
+        expect(file.ehsize).to.equal(52);
+        expect(file.phentsize).to.equal(32);
+        expect(file.phnum).to.equal(7);
+        expect(file.shentsize).to.equal(40);
+        expect(file.shnum).to.equal(28);
+        expect(file.shstrndx).to.equal(25);
+        done();
+      });
+    });
+  });
+  
   describe('loading a Sifteo SVM ELF file', function() {
-    
     it('should parse correctly', function(done) {
       elf.load('test/data/sifteo-cubes/membrane.elf', function(err, file) {
         expect(err).to.be.null;
